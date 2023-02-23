3b4bae4dd0841303a2cccfa90785b704a515c204
Jshan02 <jyeshan3770@gmail.com>
2023-02-24 02:36:18 +0800

    usercommonmethod

---
+++ src/main/java/User/UserCommonMethod.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+
+/**
+ *
+ * @author user
+ */
+public abstract class UserCommonMethod {
+    public abstract String getUsername();
+    public abstract String getPassword();
+    public abstract void changePassword(String uname, String pw, String newPW);
+}
